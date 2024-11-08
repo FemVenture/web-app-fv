2ddc3e0a585294b6e123b6e4d452c621a9cd5aea
asotito <84928376+asotito@users.noreply.github.com>
2024-11-08 00:22:32 -0500

    feat: created buttom component to be used in every button

--- src/public/pages/Home.tsx
+++ src/public/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { ReactElement } from "react";
+import { Button } from "../../shared/components/button";
 
 export const Home = (): ReactElement => {
     return (
         <div>
             <h1>Home Page</h1>
+            <Button label="Boton" type="button" size="large" variant="primary" />
+            <Button label="Boton" type="button" size="large" variant="secondary" borderColor="gray" borderSize="big" />
         </div>
     );
 }
